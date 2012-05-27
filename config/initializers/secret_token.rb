95210fb8f4f7b27010fc120e3e479d92c60aa2bb
adamjacobbecker <ad@mbecker.cc>
2012-05-26 21:10:34 -0700

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HubSlideshows::Application.config.secret_token = '017f3d7e1acd4bd3cd5b0fce74c24c30d941cd3b499d5208e1f140bd1a39a39c7126d59da83131a6be4737fc29a14128aa51ca9c2ba6bd2c001972b3811935a5'
