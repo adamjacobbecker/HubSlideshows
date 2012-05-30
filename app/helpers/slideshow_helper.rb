903da139d57982b9de3bb3e30d894716339a4ed3
adamjacobbecker <ad@mbecker.cc>
2012-05-29 17:36:40 -0700

    new slide format

--- app/helpers/slideshow_helper.rb
+++ app/helpers/slideshow_helper.rb
@@ -24,35 +24,17 @@ module SlideshowHelper
     @slide_params[:page_count] += 1
   end
 
-  def after_project
+  def after_project    
+ 
+    @slide_params[:x] = @slide_params[:orig_x] + 3500
     
-    @slide_params["total_pages_for_#{@slide_params[:project_count]}"] = @slide_params[:page_count]
-    @slide_params["max_y_for_#{@slide_params[:project_count]}"] = @slide_params[:y]
-    
-    @last_row_most_slides = ([@slide_params["total_pages_for_#{@slide_params[:project_count]}"],
-                             (@slide_params["total_pages_for_#{@slide_params[:project_count] - 1}"] || 1)]).max
-                             
-    @last_row_max_y = ([@slide_params["max_y_for_#{@slide_params[:project_count]}"],
-                            (@slide_params["max_y_for_#{@slide_params[:project_count] - 1}"] || 1)]).max
-    
-                        
-    if @last_row_most_slides < 3
-      @last_row_height = 1000
-    elsif @last_row_most_slides < 6
-      @last_row_height = 1400
-    elsif @last_row_most_slides < 9
-      @last_row_height = 1400
-    else
-      @last_row_height = 1700
+    if @slide_params[:page_count] == 3
+      @slide_params[:x] -= 1000
+    elsif @slide_params[:page_count] <= 2
+      @slide_params[:x] -= 1500      
     end
     
-    if @slide_params[:project_count] % 2 == 0
-      @slide_params[:x] = 0
-      @slide_params[:y] = @last_row_max_y + @last_row_height
-    else
-      @slide_params[:x] = @slide_params[:orig_x] + 3500
-      @slide_params[:y] = @slide_params[:orig_y]   
-    end
+    @slide_params[:y] = @slide_params[:orig_y]   
     
     @slide_params[:page_count] = 1
     @slide_params[:project_count] += 1
