eef809c87a3622fd8a277b01dfec6dc53700d0ca
adamjacobbecker <ad@mbecker.cc>
2012-06-16 10:07:48 -0700

    remove uploads/ directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ if Rails.env.production?
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
       :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
     }
-    config.fog_directory  = 'HubSlideshows/uploads/'
+    config.fog_directory  = 'HubSlideshows'
   end
   
 else
